1bed1c24db54c8ec50652e01ec4a79a715aa5e8d
MIN HTET NAING <73007388+minhtet97@users.noreply.github.com>
2022-10-14 23:20:07 +0900

    boom test

--- Assets/Users/min/gamemanager.cs
+++ Assets/Users/min/gamemanager.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class min : MonoBehaviour
+public class gamemanager : MonoBehaviour
 {
+    public Transform outpos;
+    public GameObject Boom;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,9 @@ public class min : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(Boom, outpos.position,Quaternion.identity);
+        }
     }
 }
