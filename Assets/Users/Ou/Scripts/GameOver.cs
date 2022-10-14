685d51b6217bc378479ba15c7cdb921d2cf2cf68
jiangyouxiaosheng <73007788+jiangyouxiaosheng@users.noreply.github.com>
2022-10-15 03:56:05 +0900

    ou
    player

---
+++ Assets/Users/Ou/Scripts/GameOver.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+   
+   
+   private void OnTriggerEnter2D(Collider2D col)
+   {
+     if(col.CompareTag("Player")) 
+      {
+         Debug.Log("Game over");
+      }
+   }
+}
--- Assets/Users/Ou/Scripts/Player.cs
+++ Assets/Users/Ou/Scripts/Player.cs
@@ -4,53 +4,71 @@ using System.Collections.Generic;
 using System.Drawing;
 using UnityEngine;
 using UnityEngine.Experimental.Playables;
+using UnityEngine.SearchService;
 
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb => GetComponent<Rigidbody2D>();
+    private Animator anim => GetComponent<Animator>();
 
     public float jumpForce;
-
-    public float jumpNum;
+    public int  jumpNum;
+    private int jumpCurreNum;
     private float speed=10f;
     private float andPlayer;
     public Transform playerPoint;
     public LayerMask layer;
+    private bool isThis;
 
     private CircleCollider2D _circleCollider2D=>GetComponent<CircleCollider2D>();
 
     private bool isGround;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        jumpCurreNum = jumpNum;
     }
 
     // Update is called once per frame
     void Update()
     {
+        ChickGround();
         AndPlayer();
-        if (Input.GetKeyDown(KeyCode.Space)&&jumpNum >0)
+        if (Input.GetKeyDown(KeyCode.Space)&& jumpCurreNum >0)
         {
+            anim.SetBool("IsGround",false);
             rb.velocity += Vector2.up*jumpForce;
-            jumpNum--;
+            jumpCurreNum--;
+            anim.SetTrigger("IsJump");
         }
+        
+
+
+
+
 
         if (andPlayer > .1)
         {
             this.transform.position = Vector2.MoveTowards(transform.position, playerPoint.position,
                 speed * Time.deltaTime);
         }
+       
+       
         
-        ChickGround();
     }
 
     private void ChickGround()
     {
         isGround = Physics2D.IsTouchingLayers(_circleCollider2D, layer);
+      
+       
         if (isGround)
         {
-            jumpNum = 1;
+           
+            jumpCurreNum = jumpNum;
+            anim.SetBool("IsGround",true);
         }
     }
 
@@ -59,8 +77,5 @@ public class Player : MonoBehaviour
         andPlayer = Vector3.Distance(this.transform.position, playerPoint.transform.position);
     }
 
-    private void OnTriggerEnter2D(Collider2D col)
-    {
-        
-    }
+ 
 }
